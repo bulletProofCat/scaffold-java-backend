a7bd3db52308a0f60e1e46a936c8c6de8aef8f9e
gaolinfeng <huiqin.glf@autonavi.com>
2020-08-23 18:27:09 +0800

    trying to add shiro support

---
+++ sql/ddl/user.sql
@@ -0,0 +1,60 @@
+--create database test CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+
+drop table if exists user;
+drop table if exists role;
+drop table if exists user_role;
+drop table if exists department;
+drop table if exists company;
+drop table if exists company_department;
+
+create table user (
+    id int primary key auto_increment,
+    phone varchar(20) UNIQUE NOT NULL,
+    email varchar(100) UNIQUE NOT NULL,
+    name varchar(40) NOT NULL,
+    password varchar(128) NOT NULL COMMENT '密码+salt计算的hash',
+    salt varchar(128) NOT NULL COMMENT '密码的salt',
+    sex int NOT NULL COMMENT '0表示未指定，1男2女',
+    avatar varchar(400) NOT NULL,
+    department_id int NOT NULL,
+    create_time datetime NOT NULL DEFAULT current_timestamp,
+    update_time datetime NOT NULL DEFAULT current_timestamp ON UPDATE current_timestamp
+);
+
+create table role (
+    id int primary key auto_increment,
+    name varchar(40) NOT NULL,
+    description varchar(200) NOT NULL,
+    create_time datetime NOT NULL DEFAULT current_timestamp,
+    update_time datetime NOT NULL DEFAULT current_timestamp ON UPDATE current_timestamp
+);
+
+create table user_role (
+    id int primary key auto_increment,
+    user_id int NOT NULL,
+    role_id int NOT NULL,
+    create_time datetime NOT NULL DEFAULT current_timestamp
+);
+
+create table department (
+    id int primary key auto_increment,
+    name varchar(40) UNIQUE NOT NULL,
+    create_time datetime NOT NULL DEFAULT current_timestamp,
+    update_time datetime NOT NULL DEFAULT current_timestamp ON UPDATE current_timestamp
+);
+
+create table company (
+    id int primary key auto_increment,
+    name varchar(40) UNIQUE NOT NULL,
+    address varchar(200) NOT NULL,
+    photo varchar(400) NOT NULL,
+    create_time datetime NOT NULL DEFAULT current_timestamp,
+    update_time datetime NOT NULL DEFAULT current_timestamp ON UPDATE current_timestamp
+);
+
+create table company_department (
+    id int primary key auto_increment,
+    company_id int NOT NULL,
+    department_id int NOT NULL,
+    create_time datetime NOT NULL DEFAULT current_timestamp
+);
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,15 +2,19 @@ package com.example.demo;
 
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.core.util.StatusPrinter;
-import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.realm.Realm;
+import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
+import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -27,10 +31,8 @@ public class DemoApplication implements CommandLineRunner {
 	private UserService userService;
 
 	@Override
-	public void run(String... args) throws Exception {
-
-//		User user = userService.getUser();
-//		log.info("user: {}", user);
+	public void run(String... args) {
+		printLoggingStatus();
 	}
 
 	private void printLoggingStatus() {
@@ -41,24 +43,18 @@ public class DemoApplication implements CommandLineRunner {
 		StatusPrinter.print(lc);
 	}
 
-//	@Bean
-//	public Realm realm() {
-//		return new MyBatisRealm();
-//	}
-//	@Bean
-//	public ShiroFilterChainDefinition shiroFilterChainDefinition() {
-//		DefaultShiroFilterChainDefinition chainDefinition = new DefaultShiroFilterChainDefinition();
-//
-//		// logged in users with the 'admin' role
-//		chainDefinition.addPathDefinition("/admin/**", "authc, roles[admin]");
-//
-//		// logged in users with the 'document:read' permission
-//		chainDefinition.addPathDefinition("/docs/**", "authc, perms[document:read]");
-//
-//		// all other paths require a logged in user
-//		chainDefinition.addPathDefinition("/**", "authc");
-//		return chainDefinition;
-//	}
+	@Bean
+	public Realm realm() {
+		return null;
+	}
 
+	@Bean
+	public ShiroFilterChainDefinition shiroFilterChainDefinition() {
+		// https://shiro.apache.org/spring-boot.html#annotations-and-web-applications
+		// 接下来使用注解控制权限，因此shiro filter默认使用anon放行所有检查
+		DefaultShiroFilterChainDefinition chainDefinition = new DefaultShiroFilterChainDefinition();
+		chainDefinition.addPathDefinition("/**", "anon");
+		return chainDefinition;
+	}
 
 }
